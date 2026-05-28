6d447829b802c5c180629cf43427eeebd44302d2
Subhajit Dey <73297451+subhajitdey295@users.noreply.github.com>
2026-05-28 20:02:34 +0530

    Delete customer.sql

--- customer.sql
+++
@@ -1,9 +0,0 @@
-select item_purchased, 
-ROUND(100 * SUM(CASE WHEN discount_applied = 'Yes' THEN 1 ELSE 0 END)/COUNT(*),2) as discount_rate
-from customer
-group by item_purchased
-order by discount_rate desc
-limit 5;
-
-select *
-from customer
